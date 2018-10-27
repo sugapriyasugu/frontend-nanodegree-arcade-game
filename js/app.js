49ade51b1a09fa805c3df3cd1c2d76d33a1c5a5c
sugaselvaraj <sugaselvaraj@paypal.com>
2018-10-27 11:32:08 +0530

    add event listener to listen user inputs

--- js/app.js
+++ js/app.js
@@ -102,3 +102,16 @@ class Player {
 let player = new Player(202, 410, 55);
 let allEnemies = [];
 let enemyPosition = [63, 145, 227];
+
+// This listens for key presses and sends the keys to your
+// Player.handleInput() method. You don't need to modify this.
+document.addEventListener('keyup', function(e) {
+    var allowedKeys = {
+        37: 'left',
+        38: 'up',
+        39: 'right',
+        40: 'down'
+    };
+    player.handleInput(allowedKeys[e.keyCode]);
+});
+
